10bd28ae3135280f675552682c4ce1bbfd7be255
nyxara-tempest02 <nyxara_tempest@outlook.com>
2024-10-26 11:42:10 -0600

    Update installation_script_test.sh

--- installation_script_test.sh
+++ installation_script_test.sh
@@ -24,3 +24,34 @@ if ! command -v dialog &>/dev/null; then
   pacman -Sy --noconfirm dialog
 fi   
 
+# Template selection menu
+TEMPLATE=$(dialog --clear --backtitle "Arch Linux Installation" \
+--title "Template Selection" \
+--menu "Choose an installation template:" 15 60 4 \
+"1" "Minimal Install" \
+"2" "Cybersecurity Oriented" \
+"3" "Custom Installation" \
+3>&1 1>&2 2>&3 3>&-)
+
+if [[ -z $TEMPLATE ]]; then
+  echo -e "\e[31m[ERROR]\e[0m No template selected. Exiting."
+  exit 1
+fi
+
+echo -e "\e[32m[INFO]\e[0m Template selected: $TEMPLATE"
+
+#Get list of disks
+#mapfile -t DISKS < <(lsblk -d -o NAME,SIZE -n | awk '{print "/dev/"$1 " ("$2")"}')
+#DISK_CHOICE=$(dialog --backtitle "Arch Linux Installation" \
+#--title "Disk Selection" \
+#--menu "Select the disk to install Arch Linux on:" 15 60 ${#DISKS[@]} \
+#"${DISKS[@]}" \
+#3>&1 1>&2 2>&3 3>&-)
+
+#if [[ -z $DISK_CHOICE ]]; then
+#  echo -e "\e[31m[ERROR]\e[0m No disk selected. Exiting."
+#  exit 1
+#fi
+
+#INSTALL_DISK=$(echo $DISK_CHOICE | awk '{print $1}')
+#echo -e "\e[32m[INFO]\e[0m Disk selected: $INSTALL_DISK"
